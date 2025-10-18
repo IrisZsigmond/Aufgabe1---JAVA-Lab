226d969a57f9ebdbb33d2fcba40e898a94852dc6
IrisMaria <iris.zsigmond@stud.ubbcluj.ro>
2025-10-18 17:53:42 +0300

    Implemented first method of the grading system class: notSufficientGrades

--- src/GradingSystem.java
+++ src/GradingSystem.java
@@ -1,3 +1,13 @@
-public class GradingSystem {
+import java.util.ArrayList;
 
+public class GradingSystem {
+    public ArrayList<Integer> notSufficientGrades(ArrayList<Integer> grades) {
+        ArrayList<Integer> new_grades = new ArrayList<Integer>();
+
+        for (int grade : grades)
+            if (grade < 38)
+                new_grades.add(grade);
+
+        return new_grades;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         System.out.println(ProfessorGradeRounding.roundGrade(84)); // Output: 85
         System.out.println(ProfessorGradeRounding.roundGrade(37)); // Output: 37
         System.out.println(ProfessorGradeRounding.roundGrade(38)); // Output: 40
+
+        System.out.println("Not sufficient grades:" + new GradingSystem().notSufficientGrades(new ArrayList<Integer>(List.of(84, 37, 38, 29, 45))));
     }
 }
